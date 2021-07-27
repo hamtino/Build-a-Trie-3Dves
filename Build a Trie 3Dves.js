c5aa82f2360623b82ef17ea343a5b755c2ed08ec
Juan Jaminton Yate Pelaez <50279936+hamtino@users.noreply.github.com>
2021-07-27 14:03:22 -0500

    Add files via upload

---
+++ Build a Trie 3Dves.js
@@ -0,0 +1,60 @@
+function buildTrie(...words) {
+    const map1 = new Map();
+    let string = "";
+    for (let i = 0; i < words.length; i++) {
+      let objeto = map1.get(words[i][0]);
+      if (objeto != undefined) {
+        let palabra = words[i];
+        for (let j = 0; j < words[i].length; j++) {
+          if (j == 0) {
+            string = '{"' + palabra + '": null}';
+          } else {
+            if (palabra.length > 2) {
+              palabra = palabra.substring(0, palabra.length - 1);
+              string = '{"' + palabra + '": ' + string + "}";
+            }
+          }
+          if (words[i].length != 1) {
+          }
+        }
+        let buscar = objeto.te;
+        if (buscar != undefined) {      
+            JSON.stringify(Object.assign(objeto.te, JSON.parse(string).te)
+          );
+        } else {      
+  
+            JSON.stringify(Object.assign(objeto, JSON.parse(string))
+          );
+        }
+      } else {
+        let palabra = words[i];
+        for (let j = 0; j < words[i].length; j++) {
+          if (words[i].length == 1) {
+            map1.set(words[i][0], null);
+          } else {
+            if (j == 0) {
+              string = '{"' + palabra + '": null}';
+            } else {
+              if (palabra.length > 2) {
+                palabra = palabra.substring(0, palabra.length - 1);
+                string = '{"' + palabra + '": ' + string + "}";
+                
+              }
+            }
+          }
+          if (words[i].length != 1) {
+            map1.set(words[i][0], JSON.parse(string));
+          }
+        } 
+      }
+    }
+    const obj = mapToObj(map1);
+    function mapToObj(map) {
+      const obj = {};
+      for (let [k, v] of map) obj[k] = v;
+      return obj;
+    }
+    console.log(map1);
+    return obj;
+  }
+  
